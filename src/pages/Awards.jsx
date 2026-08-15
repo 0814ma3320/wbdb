d52701b4cece57ef9ede1fb322bd6756d7e469ce
0814ma3320 <macha0814sas@icloud.com>
2026-08-15 17:52:58 +0900

    fix awards import

--- src/pages/Awards.jsx
+++ src/pages/Awards.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { leaguePlayers } from "../data/LeaguePlayers";
+import { leaguePlayers } from "../data/leaguePlayers";
 
 import { getPlayersForSeason } from "../data/playerStorage";
 import {
